781ad217d5896c706ef71864aaa98c6ea87c0643
naatrada <l4natrada@gmail.com>
2018-10-31 15:27:49 +0700

    message

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -24,7 +24,7 @@
       <b-carousel-slide
           caption="General Candles" 
           img-src="https://www.girlsallaround.com/wp-content/uploads/2016/04/ti_725_46923805513341-600x337.jpg">
-        <h1>Hello world!</h1>
+         
       </b-carousel-slide>
 
       <!-- Slides with image only -->
@@ -47,13 +47,8 @@
       </b-carousel-slide>
 
     </b-carousel></b-col>
-        <b-col><b-row><h1>The candle</h1></b-row><b-row>A candle is an ignitable wick embedded in wax, or another flammable solid substance such as tallow, that provides light, and in some cases, a fragrance. A candle can also provide heat, or be used as a method of keeping time.
-
-A person who makes candles is traditionally known as a chandler.[1] Various devices have been invented to hold candles, from simple tabletop candlesticks, also known as candle holders, to elaborate chandeliers.[2]
-
-For a candle to burn, a heat source (commonly a naked flame) is used to light the candle's wick, which melts and vaporizes a small amount of fuel (the wax). Once vaporized, the fuel combines with oxygen in the atmosphere to ignite and form a constant flame. This flame provides sufficient heat to keep the candle burning via a self-sustaining chain of events: the heat of the flame melts the top of the mass of solid fuel; the liquefied fuel then moves upward through the wick via capillary action; the liquefied fuel finally vaporizes to burn within the candle's flame.
-
-As the solid fuel (wax) is melted and burned, the candle becomes shorter. Portions of the wick that are not emitting vaporized fuel are consumed in the flame. The incineration of the wick limits the exposed length of the wick, thus maintaining a constant burning temperature and rate of fuel consumption. Some wicks require regular trimming with scissors (or a specialized wick trimmer), usually to about one-quarter inch (~0.7 cm), to promote slower, steady burning, and also to prevent smoking. Special candle-scissors called "snuffers" were produced for this purpose in the 20th century and were often combined with an extinguisher. In modern candles, the wick is constructed so that it curves over as it burns. This ensures that the end of the wick gets oxygen and is then consumed by fire—a self-trimming wick.</b-row></b-col>
+        <b-col><b-row><h1>The candle</h1></b-row><b-row> Aroma candles are made with essential oils and have aromatherapeutic properties. That is to say, aroma candles are made for holistic therapeutic care — for providing well-being to the body, mind, and spirit. Aromatherapy is a curative that people have, for centuries, used to gain energy, relieve stress, enhance concentration, and manage pain. The fragrances of essential oils are emanated in order to stimulate the senses. Some aromas can even lower systolic blood pressure. Basically, aroma candles are used, not just for their pleasant fragrances, but also for their ability to change our mood 
+          — to help us feel good. </b-row></b-col>
     </b-row>
    
 
