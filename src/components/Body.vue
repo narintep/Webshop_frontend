eed304b4c3de37cdad40aad8fb86a6625b82c641
naatrada <l4natrada@gmail.com>
2018-10-31 15:14:20 +0700

    another

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -35,18 +35,15 @@
 
       <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
-             src="https://picsum.photos/1024/480/?image=55" alt="image slot">
+      <b-carousel-slide 
+          caption="Geoshape Candles"
+          img-src="https://patcharachat5651.files.wordpress.com/2014/02/benjawan.jpg">
       </b-carousel-slide>
 
       <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
-          ut lacus vel interdum.
-        </p>
+      <b-carousel-slide 
+          caption="Geoshape Candles"
+          img-src="https://3.bp.blogspot.com/-IYtFeT7WDr0/WOIEua7C4oI/AAAAAAAAEOM/5kPEUl85sMUXtQw8SUxxcsFmK8wx0wRlwCLcB/s320/%25E0%25B8%2587%25E0%25B8%25B2%25E0%25B8%2599%25E0%25B9%2580%25E0%25B8%2597%25E0%25B8%25B5%25E0%25B8%25A2%25E0%25B8%2599%25E0%25B8%25AB%25E0%25B8%25AD%25E0%25B8%25A1%2B%25E0%25B8%25AA%25E0%25B8%25B3%25E0%25B8%25AB%25E0%25B8%25A3%25E0%25B8%25B1%25E0%25B8%259A%25E0%25B8%259C%25E0%25B8%25B9%25E0%25B9%2589%25E0%25B8%2597%25E0%25B8%25B5%25E0%25B9%2588%25E0%25B8%25AB%25E0%25B8%25B2%25E0%25B8%2587%25E0%25B8%25B2%25E0%25B8%2599%25E0%25B8%2597%25E0%25B9%258D%25E0%25B8%25B2%25E0%25B8%2597%25E0%25B8%25B5%25E0%25B9%2588%25E0%25B8%259A%25E0%25B9%2589%25E0%25B8%25B2%25E0%25B8%2599%2B%25E0%25B9%2580%25E0%25B8%259B%25E0%25B9%2587%25E0%25B8%2599%25E0%25B8%25A3%25E0%25B8%25B2%25E0%25B8%25A2%25E0%25B9%2584%25E0%25B8%2594%25E0%25B9%2589%25E0%25B9%2580%25E0%25B8%25AA%25E0%25B8%25A3%25E0%25B8%25B4%25E0%25B8%25A1%25E0%25B8%2588%25E0%25B8%25B2%25E0%25B8%2581%25E0%25B8%2587%25E0%25B8%25B2%25E0%25B8%2599%25E0%25B8%259D%25E0%25B8%25B5%25E0%25B8%25A1%25E0%25B8%25B7%25E0%25B8%25AD.jpg">
       </b-carousel-slide>
 
     </b-carousel></b-col>
