729d539e587f3a482529caff013a347760c3c9e8
naatrada <l4natrada@gmail.com>
2018-11-02 23:19:36 +0700

    detail

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Nav1 v-on:clicked="setState" />
     <Body v-if="state==0" />
-    <List v-if="state==1" v-on:clicked="setState" v-bind:arr="list" />
-    <Detail v-if="state==2" input="" />
+    <List v-if="state==1" v-on:clicked="setState2"  v-bind:arr="list" />
+    <Detail v-if="state==2" :input="id" />
     <!-- <Nav2 /> -->
   </div>
 </template>
@@ -19,16 +19,20 @@ export default {
   data() {
     return {
       state: 0,
+      id:0,
       list: this.fetchData()
     };
   },
   methods: {
     setState(value) {
       this.state = value;
+    },setState2(value) {
+      this.state = value[0];
+      this.id=value[1]
     },
     fetchData() {
       var arr = [];
-      fetch("http://localhost:3000/api/artists")
+      fetch("http://localhost:3000/api/PurchasedItem")
         .then(function(data) {
           return data.json();
         })
