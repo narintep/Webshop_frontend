01d69b6efd1e8d7414a332075a868eff87e15ac1
Narintep <nessix_tap@hotmail.com>
2018-11-04 01:53:43 +0700

    waitlist

--- src/components/WaitingList.vue
+++ src/components/WaitingList.vue
@@ -2,17 +2,17 @@
   <div class="hello">
       <b-row style="margin-left:25px;">
       <div class="mar" v-for="(i,index) in realList"  :key="index">
-  <b-card :title="i.candle_type.name"
+  <b-card :title="i.purchased_item.candle_type.name"
           img-alt="Image"
           img-top
           tag="article"
           style="max-width: 13rem;"
           class="mb-2">
-           <img :src="require('./candle/'+i.candle_type.name+'/'+i.size_id+'.png')" width=180 height="200">
+           <img :src="require('./candle/'+i.purchased_item.candle_type.name+'/'+i.purchased_item.size_id+'.png')" width=180 height="200">
           
-    <p class="card-text">size:{{i.min_weight}} <br>  price:{{i.price}} </p>
+    <p class="card-text">Amout :{{i.number}} <br>  DateIn: {{i.date_in}} <br>  DateOut: {{i.date_out}}  </p>
     
-    <b-button variant="primary" v-on:click="onClick(2,i.id)">Detail</b-button>
+    <!-- <b-button variant="primary" v-on:click="onClick(2,i.id)">Detail</b-button> -->
   </b-card>
 </div>
 </b-row>
@@ -25,7 +25,7 @@
 
 <script>
 var arr = [];
-fetch("http://localhost:3000/api/waiting_list ")
+fetch("http://localhost:3000/api/waiting_list")
   .then(function(data) {
     return data.json();
   })
