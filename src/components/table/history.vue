8bf7fe0ccf9d77072d5b2e8f7b62a3a10367c52a
kalasour <thekalasour@outlook.co.th>
2018-12-02 23:21:04 +0700

    Done history in db.

--- src/components/table/history.vue
+++ src/components/table/history.vue
@@ -1,158 +1,135 @@
 <template>
 
-<div>
+  <div>
     <!-- <div class="mar" v-for="(i,index) in realList"  :key="index"> -->
 
-  <!-- Using modifiers -->
-  <b-btn v-b-toggle.collapse7 class="m-1">History</b-btn>
-  <!-- element to collapse -->
-  
-  
-  <b-collapse id="collapse7">
-    <b-card>
-       <b-table striped hover :items="datas" :fields="fields">
-         <template slot="edit" slot-scope="row">
-      <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
-      <b-button size="sm" @click.stop="row.toggleDetails" variant="primary" class="mr-2">
-        edit
-      </b-button>
-    </template>
-    <template slot="delete" slot-scope="row">
-      <b-button size="sm" variant="danger" class="mr-2"> Delete
-      </b-button>
+    <!-- Using modifiers -->
+    <b-btn v-b-toggle.collapse7 class="m-1">History</b-btn>
+    <!-- element to collapse -->
 
-    </template>
-<template slot="row-details" slot-scope="row">
+
+    <b-collapse id="collapse7">
       <b-card>
-        <b-row class="mb-2">
-           <b-form-group id="exampleInputGroup1"
-                    label="Customer_id:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="number"
-                      v-model="row.item.customers_id"
-                      required disabled
-                      placeholder="Enter Customer_id">
-        </b-form-input>
-      </b-form-group>
+        <b-table striped hover :items="datas" :fields="fields">
+          <template slot="edit" slot-scope="row">
+            <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
+            <b-button size="sm" @click.stop="row.toggleDetails" variant="primary" class="mr-2">
+              edit
+            </b-button>
+          </template>
+          <template slot="delete" slot-scope="row">
+            <b-button size="sm" variant="danger" @click="deleteRow(row.item)" class="mr-2"> Delete
+            </b-button>
 
-      <b-form-group id="exampleInputGroup1"
-                    label="Customers_username:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="text"
-                      v-model="row.item.customers_username"
-                      required disabled
-                      placeholder="Enter Customers_username">
-        </b-form-input>
-      </b-form-group>
+          </template>
+          <template slot="row-details" slot-scope="row">
+            <b-card>
+              <b-row class="mb-2">
+                <b-form-group id="exampleInputGroup1" label="Customer_id:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="number" v-model="row.item.customers_id" required disabled
+                    placeholder="Enter Customer_id">
+                  </b-form-input>
+                </b-form-group>
 
-      <b-form-group id="exampleInputGroup1"
-                    label="Purchased_item_id:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="number"
-                      v-model="row.item.purchased_item_id"
-                      required disabled
-                      placeholder="Enter Purchased_item_id">
-        </b-form-input>
-      </b-form-group>
+                <b-form-group id="exampleInputGroup1" label="Customers_username:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="text" v-model="row.item.customers_username" required disabled
+                    placeholder="Enter Customers_username">
+                  </b-form-input>
+                </b-form-group>
 
-      <b-form-group id="exampleInputGroup1"
-                    label="Number:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="number"
-                      v-model="row.item.number"
-                      required
-                      placeholder="Enter Number">
-        </b-form-input>
-      </b-form-group>
+                <b-form-group id="exampleInputGroup1" label="Purchased_item_id:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="number" v-model="row.item.purchased_item_id" required disabled
+                    placeholder="Enter Purchased_item_id">
+                  </b-form-input>
+                </b-form-group>
 
-      
-        </b-row>
-    
-        <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
-        <b-button size="sm" @click="apply(row)">Apply</b-button>
+                <b-form-group id="exampleInputGroup1" label="Number:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="number" v-model="row.item.number" required placeholder="Enter Number">
+                  </b-form-input>
+                </b-form-group>
+
+
+              </b-row>
+
+              <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
+        <b-button size="sm" @click="updateRow(row.item.candle_type_id,row.item)">Apply</b-button>
+            </b-card>
+          </template>
+
+
+        </b-table>
+        <b-button size="sm" v-b-toggle.Add7 variant="success" class="mr-2"> Add
+        </b-button>
+        <b-collapse id="Add7">
+          <b-card>
+            <b-form @submit="onSubmit" @reset="onReset">
+              <b-row class="mb-2">
+
+                <b-form-group id="exampleInputGroup1" label="Customer_id:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="number" v-model="item.customers_id" required
+                    placeholder="Enter Customer_id">
+                  </b-form-input>
+                </b-form-group>
+
+                <b-form-group id="exampleInputGroup1" label="Customers_username:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="text" v-model="item.customers_username" required placeholder="Enter Customers_username">
+                  </b-form-input>
+                </b-form-group>
+
+                <b-form-group id="exampleInputGroup1" label="Purchased_item_id:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="number" v-model="item.purchased_item_id" required placeholder="Enter Purchased_item_id">
+                  </b-form-input>
+                </b-form-group>
+
+                <b-form-group id="exampleInputGroup1" label="Number:" label-for="exampleInput1">
+                  <b-form-input id="exampleInput1" type="number" v-model="item.number" required placeholder="Enter Number">
+                  </b-form-input>
+                </b-form-group>
+
+
+              </b-row>
+
+              <b-button type="submit" variant="primary">Submit</b-button>
+              <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+          </b-card>
+
+        </b-collapse>
       </b-card>
-    </template>
+    </b-collapse>
+  </div>
+</template>
 
-
-       </b-table>
-       <b-button size="sm" v-b-toggle.Add7 variant="success" class="mr-2"> Add
-      </b-button>
-      <b-collapse id="Add7">
-  <b-card>
-        <b-row class="mb-2">
-           
-            <b-form-group id="exampleInputGroup1"
-                    label="Customer_id:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="number"
-                      v-model="item.customers_id"
-                      required disabled
-                      placeholder="Enter Customer_id">
-        </b-form-input>
-      </b-form-group>
-
-      <b-form-group id="exampleInputGroup1"
-                    label="Customers_username:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="text"
-                      v-model="item.customers_username"
-                      required
-                      placeholder="Enter Customers_username">
-        </b-form-input>
-      </b-form-group>
-
-      <b-form-group id="exampleInputGroup1"
-                    label="Purchased_item_id:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="number"
-                      v-model="item.purchased_item_id"
-                      required
-                      placeholder="Enter Purchased_item_id">
-        </b-form-input>
-      </b-form-group>
-
-      <b-form-group id="exampleInputGroup1"
-                    label="Number:"
-                    label-for="exampleInput1">
-        <b-form-input id="exampleInput1"
-                      type="number"
-                      v-model="item.number"
-                      required
-                      placeholder="Enter Number">
-        </b-form-input>
-      </b-form-group>
-
-
-        </b-row>
-    
-        <b-button size="sm" @click="apply()">Apply</b-button>
-      </b-card>
-
-      </b-collapse>
-    </b-card>
-  </b-collapse>
-</div>
- </template>
-
- <script>
-import axios from "axios";
-export default {
-    data(){return {item:{}}},
-  props:['datas','fields','url'],
-  methods:{
-    apply(row){
-      alert(this.url)
+<script>
+  import axios from "axios";
+  export default {
+    data() {
+      return {
+        item: {}
+      }
     },
-    add(){
-      alert(this.url)
+    props: ['datas', 'fields', 'url'],
+    methods: {
+      async deleteRow(obj) {
+        
+        await axios.delete(this.url + "/" + JSON.stringify(obj));
+        this.$emit("reFetch");
+      },
+      async updateRow(id, item) {
+        await axios.put(this.url + "/" + id, item);
+        this.$emit("reFetch");
+      },
+      async onSubmit(evt) {
+        evt.preventDefault();
+        await axios.post(this.url, this.item);
+        this.$emit("reFetch");
+        this.item = {};
+      },
+      onReset(evt) {
+        evt.preventDefault();
+        /* Reset our form values */
+        this.item = {};
+      }
     },
-},
-}
+  }
 </script>
